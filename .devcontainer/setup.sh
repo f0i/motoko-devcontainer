61abdc9251df77e57f7c846f3d0821ba92ce7d80
Martin Sigloch <f0i.git@projects.f0i.de>
2023-01-18 16:45:37 +0100

    Add vessel to install script

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -7,3 +7,10 @@ npm install -g npm@9.2.0
 
 # install dfx
 sh -ci "$(curl -fsSL https://internetcomputer.org/install.sh)"
+
+# install vessel
+(
+    cd ~/bin/
+    wget https://github.com/dfinity/vessel/releases/download/v0.6.4/vessel-linux64 -O vessel
+    chmod +x vessel
+)
